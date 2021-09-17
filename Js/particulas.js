1ed8415d5ac47855c7668734ebc36b3c71b601a2
3M4NU31007 <90530684+3M4NU31007@users.noreply.github.com>
2021-09-17 18:24:31 -0500

    Update particulas.js

--- Js/particulas.js
+++ Js/particulas.js
@@ -5,7 +5,7 @@ particlesJS(
             "value": 80,
             "density": {
               "enable": true,
-              "value_area": 800
+              "value_area": 1000
             }
           },
           "color": {
